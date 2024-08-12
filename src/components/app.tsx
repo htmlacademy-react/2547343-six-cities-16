0fbdb780f5f4d09d18add47a9fd07daaf8690cf0
Katherine <katerina0708@gmail.com>
2024-08-12 13:47:02 +0300

    Правки
    Dispatch и state обернуты в хуки

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -10,8 +10,10 @@ import ErrorScreen from '../pages/error';
 import { AppRoute, AuthorizationStatus } from '../constants';
 import PrivateRoute from './private-route/private-route';
 import { FavoritesDataType, CityDataType, ReviewItemType } from '../types';
-import { store, setOffers, selectOffers } from '../store';
+import { setOffers, selectOffers } from '../store';
+import { useAppDispatch, useAppSelector } from '../hooks';
 import { offerMocks } from '../mocks/offer';
+import { useEffect } from 'react';
 
 type AppProps = {
   cities: { id: string; name: string }[];
@@ -22,19 +24,17 @@ type AppProps = {
 
 function App({ cities, favoritesData, citiesData, reviewData }: AppProps): JSX.Element {
 
-  // Лектор на разборе домашней работы сделал загрузку данных через useEffect,
-  // но что так, что так данные перезгружаются на любое движение,
-  // это не важно потому что потом мы будем их выгружать с сервера и это исправим
-  // или это сейчас уже надо было сделать иначе?
+  const dispatch = useAppDispatch();
 
-  const dispatch = store.dispatch;
-  dispatch(setOffers(offerMocks));
+  useEffect(() => {
+    dispatch(setOffers(offerMocks));
+  });
 
   const router = createBrowserRouter([
     {
       path: AppRoute.Main,
       element:
-        <MainScreen cities={cities} hasNavigation offersData={selectOffers(store.getState())} citiesData={citiesData} />,
+        <MainScreen cities={cities} hasNavigation offersData={useAppSelector(selectOffers)} citiesData={citiesData} />,
       errorElement: <ErrorScreen />,
     },
     {
--- src/components/locations-list/locations-list.tsx
+++ src/components/locations-list/locations-list.tsx
@@ -1,14 +1,10 @@
 import LocationItem from '../location-item/location-item';
-import { store, setCity } from '../../store';
-
 type LocationItemProps = {
   cities: { id: string; name: string }[];
   activeCity: string;
 }
 
 function LocationsList({ cities, activeCity }: LocationItemProps) {
-  const dispatch = store.dispatch;
-  dispatch(setCity(activeCity));
 
   const list = cities.map((item: { id: string; name: string }) => <LocationItem city={item} key={item.id} activeCity={activeCity} />);
   return (
